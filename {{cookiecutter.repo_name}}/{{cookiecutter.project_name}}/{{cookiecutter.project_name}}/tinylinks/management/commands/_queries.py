8ef043d91c656ab74ed0eeadc22ecdf3f638d69f
Fareck Allony <feydi.allony@kuwaitnet.com>
2021-01-25 13:08:37 +0200

    removed added by mistake tinylinks directory

--- {{cookiecutter.repo_name}}/{{cookiecutter.project_name}}/{{cookiecutter.project_name}}/tinylinks/management/commands/_queries.py
+++
@@ -1,5 +0,0 @@
-TINYLINK_QUERY = """ SELECT url, keyword
-                     FROM yourls_url;"""
-
-TINYLINKLOG_QUERY = """ SELECT referrer, user_agent, ip_address, click_time
-                        FROM yourls_log;"""
